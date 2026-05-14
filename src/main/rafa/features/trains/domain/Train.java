0573937c737ecc6e81575dd2be00aadc551f964f
Francisco Rafael <Raxes15zx@gmail.com>
2026-05-14 16:54:46 +0200

    se implementa el modelo de train

---
+++ src/main/rafa/features/trains/domain/Train.java
@@ -0,0 +1,13 @@
+package main.rafa.features.trains.domain; //cambiar por la ruta correcta
+
+public class Train {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
